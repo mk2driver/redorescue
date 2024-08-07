8a194ddd5b861cad2044d61b03f0854a090e5eb8
mk2driver <109166652+mk2driver@users.noreply.github.com>
2024-08-07 11:13:05 +0100

    Update functions.inc.php

--- overlay/rootdir/var/www/html/functions.inc.php
+++ overlay/rootdir/var/www/html/functions.inc.php
@@ -156,11 +156,12 @@ function get_disk_options($disks, $type_filter='/(^disk)/') {
 				foreach ($d->children as $c) {
 					if (str_starts_with($c->type, 'raid') && !array_key_exists($c->name, $raid)){
 						//raid volume found so format description and save md id as option
+						$c->tran = strtoupper($c->tran);
 						$os = NULL; if (property_exists($d, 'os')) $os = $d->os;
 						$raid_desc = $c->size; //size of raid volume
 						$raid_desc .= (empty($d->tran)?"":" $d->tran"); //interface of parent disk
-						$raid_desc .= (empty($c->type)?"":" strtoupper($c->type")); //raid type of raid volume
-						$raid_desc .= ", Software RAID Volume"
+						$raid_desc .= (empty($c->type)?"":" $c->type"); //raid type of raid volume
+						$raid_desc .= ", Software RAID Volume";
 						$raid[$c->name] = $c->name . ": " . $raid_desc . (empty($os)?"":", $os");
 					}
 				}
