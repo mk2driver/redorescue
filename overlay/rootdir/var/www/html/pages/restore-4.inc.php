bfda390db47cb0870d1157c50b2504a23c06fe33
mk2driver <109166652+mk2driver@users.noreply.github.com>
2024-08-23 18:37:27 +0100

    Update restore-4.inc.php

--- overlay/rootdir/var/www/html/pages/restore-4.inc.php
+++ overlay/rootdir/var/www/html/pages/restore-4.inc.php
@@ -39,10 +39,10 @@ foreach ($image->parts as $name=>$p) {if ($p->type == 'Whole Disk') $whole_disk_
 
 <h1>Restore</h1>
 <h3>Step 4: Choose restore options</h3>
-<p>Select which partition's data of the backup image to restore:</p>
+<p>Select which partition's data to restore:</p>
 <p>NOTE: If using the 'Full System Recovery' tab, but restoring selected partitions only, the original disk's boot record and partition table will also be copied to the destination 
-disk.  This will ensure the partition structure on the new disk is the same as the original.  The partition options below will set whether the DATA inside those partitions is 
-restored.  If using the 'Restore Data Only' tab, the boot record and partition table is NOT restored.</p>
+disk.  This will ensure the partition structure on the new disk is the same as the original.  The partition options below will set whether the DATA inside those partitions is also 
+restored.  If using the 'Restore Data Only' tab, the boot record and partition table is not restored.</p>
 
 <form id="redo_form" class="form-horizontal">
 
